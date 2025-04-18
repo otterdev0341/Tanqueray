17a5e072a0c91649ea0438350ae63e856ee664ab
Otterdev_0341 <128258559+otterdev0341@users.noreply.github.com>
2025-04-18 22:46:28 +0700

    initial project with tailwind

---
+++ tanqueray/src/App.tsx
@@ -0,0 +1,12 @@
+
+import "./App.css";
+
+function App() {
+  return(
+    <div class="container mx-auto bg-red-200">
+        <p class="text-2xl text-green-400">Hello new project</p>
+    </div>
+  )
+}
+
+export default App;
